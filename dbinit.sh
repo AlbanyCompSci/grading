eb8863e91f15eabbdc12e1c4512114ea3eac9650
Randall Ma <randall@randallma.com>
2014-05-19 17:29:33 -0700

    add fixtures

---
+++ dbinit.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cd grading
+
+rm db.sqlite3
+python manage.py syncdb
+python manage.py migrate usermanage
+python manage.py migrate lessons
+python manage.py loaddata usermanage
+python manage.py loaddata lessons
